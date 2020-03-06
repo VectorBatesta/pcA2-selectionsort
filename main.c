348f091944a80d3bf82d25ffbd152766dde1fd84
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2020-03-06 10:15:56 -0300

    correcto

--- main.c
+++ main.c
@@ -5,15 +5,20 @@ void selectionsort(int *vetor, int tamvetor){
   for(int seta = 0; seta < tamvetor; seta++){
     int menor = seta;
 
-    int i = seta;
-    for(; i < tamvetor; i++){
+    for(int i = seta; i < tamvetor; i++){
       if(vetor[i] < vetor[menor])
         menor = i;
     }
 
     int aux = vetor[seta];
-    vetor[seta] = vetor[i];
-    vetor[i] = aux;
+    vetor[seta] = vetor[menor];
+    vetor[menor] = aux;
+
+    printf("iteracao %i: ", seta+1);
+    for(int i = 0; i < tamvetor; i++){
+      printf("%i, ", vetor[i]);
+    }
+    printf("\n");
   }
 }
 
@@ -25,15 +30,16 @@ int main(){
     printf("%i,\n", vetor[i]);
   }
 
-  printf("\n[pressione ENTER para continuar]");
+  printf("\n[pressione ENTER para continuar]\n");
   getchar();
 
   selectionsort(vetor, 10);
 
-  printf("vetor SELECTIONSORTADO:\n");
+  printf("\n\nvetor SELECTIONSORTADO:\n");
   for(int i = 0; i < tamvetor; i++){
-    printf("%i,\n", vetor[i]);
+    printf("%i, ", vetor[i]);
   }
+  printf("\n");
 
   return 0;
 }
